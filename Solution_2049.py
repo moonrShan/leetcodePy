4a3a906e932a9031e8753ef59ece60e5a463d5b1
moonrShan <moonrise.js@gmail.com>
2021-10-24 10:16:10 -0700

    F

--- Solution_2049.py
+++ Solution_2049.py
@@ -1,19 +1,26 @@
-
 class Solution_2049:
     def countHighestScoreNodes(self, parents: List[int]) -> int:
-        tree = [[] for _ in parents]
-        for i, x in enumerate(parents):
-            if x >= 0: tree[x].append(i)
+        self.graph = collections.defaultdict(list)
+        self.children = collections.defaultdict(list)
+        for i in range(len(parents)):
+            if parents[i] >= 0:
+                self.graph[parents[i]].append(i)
+        self.dfsCountChildren(0)
 
-        freq = defaultdict(int)
+        result = 1
+        total = len(parents)
+        for i in range(len(parents)):
+            [left,right]= self.children[i]
+            result = max(result, max(left, 1) * max(right, 1) * max(total - left - right - 1, 1))
 
-        def fn(x):
-            left = right = 0
-            if tree[x]: left = fn(tree[x][0])
-            if len(tree[x]) > 1: right = fn(tree[x][1])
-            score = (left or 1) * (right or 1) * (len(parents) - 1 - left - right or 1)
-            freq[score] += 1
-            return 1 + left + right
+        return result
 
-        fn(0)
-        return freq[max(freq)]
+    def dfsCountChildren(self, node):
+        child = self.graph[node]
+        if not child or len(child) == 0:
+            self.children[node] = [0, 0]
+        if len(child) == 1:
+            self.children[node] = [1 + self.dfsCountChildren(child[0]), 0]
+        if len(child) == 2:
+            self.children[node] = [1 + self.dfsCountChildren(child[0]), 1 + self.dfsCountChildren(child[1])]
+        return sum(self.children[node])
